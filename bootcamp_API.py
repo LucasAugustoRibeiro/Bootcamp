e62a18aa0fd6185f269c45f1c2de150abeb30920
LucasAugustoRibeiro <101156853+LucasAugustoRibeiro@users.noreply.github.com>
2022-09-03 11:21:21 -0300

    Update bootcamp_API.py

--- bootcamp_API.py
+++ bootcamp_API.py
@@ -34,29 +34,7 @@ def main():
 
 
     ID_Cliente = st.text_input("ID do Cliente")
-    maiorAtraso = st.text_input("Maior atraso")
-    margemBrutaAcumulada = st.text_input("Margem Bruta Acm.")
-    percentualProtestos = st.text_input("Percentual de protestos (%)")
-    prazoMedioRecebimentoVendas = st.text_input("Prazo médio de recebimento")
-    titulosEmAberto = st.text_input("Títulos em Aberto")
     valorSolicitado = st.text_input("Valor de crédito solicitado")
-    percentualRisco = st.text_input("Percentual de Risco")
-    ativoCirculante = st.text_input("Ativo Circulante")
-    passivoCirculante = st.text_input("Passivo Circulante")
-    totalAtivo = st.text_input("Ativo Total")
-    totalPatrimonioLiquido = st.text_input("Patrimonio Liquido Total")
-    endividamento = st.text_input("Endividamento total")
-    duplicatasAReceber = st.text_input("Duplicatas a Receber")
-    estoque = st.text_input("Estoque declarado")
-    faturamentoBruto = st.text_input("Faturamento Bruto")
-    margemBruta = st.text_input("Margem Bruta")
-    custos = st.text_input("Custos")
-    capitalSocial = st.text_input("Capital Social")
-    scorePontualidade = st.text_input("Score de Pontualidade")
-    idadeNaSolicitacao = st.text_input("Idade da empresa")
-    primeiraCompra_Anos = st.text_input("Ano da primeira compra")
-    restricoes_True = st.text_input("Possui restrições? (1 = Sim | 0 = Não)")
-    empresa_MeEppMei_True = st.text_input("É ME EPP Mei? (1 = Sim | 0 = Não)")
 
 
     warn_html ="""  
